68d24f3ffa728d9f18a0522b6689eca10b0f4788
tmknom <tmk.nom@gmail.com>
2018-12-16 18:55:00 +0900

    Implement ECS AWS CodeDeploy IAM Role

--- main.tf
+++ main.tf
@@ -1,3 +1,126 @@
 # Terraform module which creates CodeDeploy for ECS resources on AWS.
 #
 # https://docs.aws.amazon.com/codedeploy/latest/userguide/welcome.html
+
+# ECS AWS CodeDeploy IAM Role
+#
+# https://docs.aws.amazon.com/ja_jp/AmazonECS/latest/developerguide/codedeploy_IAM_role.html
+
+# https://www.terraform.io/docs/providers/aws/r/iam_role.html
+resource "aws_iam_role" "default" {
+  name               = "${local.iam_name}"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+  path               = "${var.ecs_codedeploy_path}"
+  description        = "${var.ecs_codedeploy_description}"
+  tags               = "${merge(map("Name", local.iam_name), var.tags)}"
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["codedeploy.amazonaws.com"]
+    }
+  }
+}
+
+# https://www.terraform.io/docs/providers/aws/r/iam_policy.html
+resource "aws_iam_policy" "default" {
+  name        = "${local.iam_name}"
+  policy      = "${data.aws_iam_policy_document.policy.json}"
+  path        = "${var.ecs_codedeploy_path}"
+  description = "${var.ecs_codedeploy_description}"
+}
+
+data "aws_iam_policy_document" "policy" {
+  # If the tasks in your Amazon ECS service using the blue/green deployment type require the use of
+  # the task execution role or a task role override, then you must add the iam:PassRole permission
+  # for each task execution role or task role override to the AWS CodeDeploy IAM role as an inline policy.
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "iam:PassRole",
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "ecs:DescribeServices",
+      "ecs:CreateTaskSet",
+      "ecs:UpdateServicePrimaryTaskSet",
+      "ecs:DeleteTaskSet",
+      "cloudwatch:DescribeAlarms",
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "sns:Publish",
+    ]
+
+    resources = ["arn:aws:sns:*:*:CodeDeployTopic_*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "elasticloadbalancing:DescribeTargetGroups",
+      "elasticloadbalancing:DescribeListeners",
+      "elasticloadbalancing:ModifyListener",
+      "elasticloadbalancing:DescribeRules",
+      "elasticloadbalancing:ModifyRule",
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "lambda:InvokeFunction",
+    ]
+
+    resources = ["arn:aws:lambda:*:*:function:CodeDeployHook_*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectMetadata",
+      "s3:GetObjectVersion",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "s3:ExistingObjectTag/UseWithCodeDeploy"
+      values   = ["true"]
+    }
+
+    resources = ["*"]
+  }
+}
+
+# https://www.terraform.io/docs/providers/aws/r/iam_role_policy_attachment.html
+resource "aws_iam_role_policy_attachment" "default" {
+  role       = "${aws_iam_role.default.name}"
+  policy_arn = "${aws_iam_policy.default.arn}"
+}
+
+locals {
+  iam_name = "${var.name}-ecs-codedeploy"
+}
--- variables.tf
+++ variables.tf
@@ -1,1 +1,22 @@
+variable "name" {
+  type        = "string"
+  description = "The name of the application."
+}
 
+variable "ecs_codedeploy_path" {
+  default     = "/"
+  type        = "string"
+  description = "Path in which to create the ecs codedeploy role and the ecs codedeploy policy."
+}
+
+variable "ecs_codedeploy_description" {
+  default     = "Managed by Terraform"
+  type        = "string"
+  description = "The description of the ecs codedeploy role and the ecs codedeploy policy."
+}
+
+variable "tags" {
+  default     = {}
+  type        = "map"
+  description = "A mapping of tags to assign to all resources."
+}
