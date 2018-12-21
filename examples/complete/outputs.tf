8abafc2a0758e3e79ff6ea7abfe9fefd950ff15b
tmknom <tmk.nom@gmail.com>
2018-12-22 01:15:55 +0900

    Implement complete example

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -1,1 +1,55 @@
+output "codedeploy_app_id" {
+  value = "${module.codedeploy.codedeploy_app_id}"
+}
 
+output "codedeploy_app_name" {
+  value = "${module.codedeploy.codedeploy_app_name}"
+}
+
+output "codedeploy_deployment_group_id" {
+  value = "${module.codedeploy.codedeploy_deployment_group_id}"
+}
+
+output "ecs_codedeploy_role_arn" {
+  value = "${module.codedeploy.ecs_codedeploy_role_arn}"
+}
+
+output "ecs_codedeploy_role_create_date" {
+  value = "${module.codedeploy.ecs_codedeploy_role_create_date}"
+}
+
+output "ecs_codedeploy_role_unique_id" {
+  value = "${module.codedeploy.ecs_codedeploy_role_unique_id}"
+}
+
+output "ecs_codedeploy_role_name" {
+  value = "${module.codedeploy.ecs_codedeploy_role_name}"
+}
+
+output "ecs_codedeploy_role_description" {
+  value = "${module.codedeploy.ecs_codedeploy_role_description}"
+}
+
+output "ecs_codedeploy_policy_id" {
+  value = "${module.codedeploy.ecs_codedeploy_policy_id}"
+}
+
+output "ecs_codedeploy_policy_arn" {
+  value = "${module.codedeploy.ecs_codedeploy_policy_arn}"
+}
+
+output "ecs_codedeploy_policy_description" {
+  value = "${module.codedeploy.ecs_codedeploy_policy_description}"
+}
+
+output "ecs_codedeploy_policy_name" {
+  value = "${module.codedeploy.ecs_codedeploy_policy_name}"
+}
+
+output "ecs_codedeploy_policy_path" {
+  value = "${module.codedeploy.ecs_codedeploy_policy_path}"
+}
+
+output "ecs_codedeploy_policy_document" {
+  value = "${module.codedeploy.ecs_codedeploy_policy_document}"
+}
